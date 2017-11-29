5780a559e0d64445a4b1c44828b85f19fe2d97dd
Mike Marlow <michael.s.marlow@gmail.com>
2017-11-29 15:22:54 -0700

    corrects indenting and semi colon inconsistencies, also corrects object data formatting to be camelCase

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const app = express();
 
 
-function findById(data, id){
-    for (let i = 0; i < data.length; i++){
-        if (data[i].ID == id){
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
             return data[i];
         }
     }
@@ -16,22 +16,22 @@ function findById(data, id){
 app.use(cors());
 
 app.get("/", function(req, res) {
-  res.json({Data: cohorts});
+    res.json({data: cohorts});
 })
 
 
 app.get("/:id", function(req, res) {
-    var idNum = findById(cohorts, req.params.id);
-    if (!idNum){
+    var currentCohort = findById(cohorts, req.params.id);
+    if (!currentCohort) {
         res.status(404);
         res.json({
             error: {
-                message: "No record found!"
+                message: "No record found!";
             }
         });
     } else {
-    res.json({Data: idNum})};
-});
+    res.json({data: currentCohort})};
+})
 
 
 app.listen(process.env.PORT || 8080);
